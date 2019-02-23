eff4e55b422a21afcc63b805c9ffe4f62e3f766a
Zhi Liu <liuzhi0210@gmail.com>
2019-02-22 21:15:50 -0800

    Implement in Flask Ask

--- webservice/greeting_ask.py
+++ webservice/greeting_ask.py
@@ -1,10 +1,18 @@
 from flask import Flask
-from flask_ask import Ask, statement, question
+from flask_ask import Ask, statement, question, session
 import datetime
 import os
+import logging
+import requests
+
 
 app = Flask(__name__)
+app.config['ASK_APPLICATION_ID'] = 'amzn1.ask.skill.ef12e20b-9428-4d86-9303-87d87e41f348'
 ask = Ask(app, '/alexa_endpoint')
+
+
+if os.environ.get('GREETING_ASK_DEBUG', '').lower() == 'true':
+    logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
 @ask.launch
 def launch():
@@ -17,11 +25,58 @@ def launch():
 def hello(first_name):
     speech_text = 'Hello {0}. '.format(first_name)
     speech_text += get_wish()
-    return statement("<speak>{}</speak>".format(speech_text))
+    quote = get_quote()
+    speech_text += ' {0}'.format(quote)
+    return statement(get_ssml(speech_text))
+
+@ask.intent('QuoteIntent')
+def quote_intent():
+    speech_text, reprompt_text = get_quote_text()
+    session.attributes['quote_intent'] = True
+    return question(speech_text).reprompt(reprompt_text)
+
+@ask.intent('NextQuoteIntent')
+def next_quote_intent():
+    if 'quote_intent' in session.attributes:
+        speech_text, reprompt_text = get_quote_text()
+        return question(speech_text).reprompt(reprompt_text)
+    else:
+        speech_text = get_ssml('Wrong invocation of this intent. Please say get me a quote to get quote.')
+        return statement(speech_text)
 
 @ask.session_ended
 def session_ended():
     return "", 200
+
+@ask.intent('AMAZON.StopIntent')
+def stop_intent():
+    speech_text = get_ssml('Good Byte.')
+    return statement(speech_text)
+
+@ask.intent('AMAZON.CancelIntent')
+def stop_intent():
+    speech_text = get_ssml('Good Byte.')
+    return statement(speech_text)
+
+
+def get_quote_text():
+    speech_text = get_quote()
+    speech_text += ' Do you want to listen to one more quote? '
+    reprompt_text = ' You can say yes or one more. '
+    speech_text = get_ssml(speech_text)
+    reprompt_text = get_ssml(reprompt_text)
+    return speech_text, reprompt_text
+
+def get_ssml(text):
+    return "<speak>{}</speak>".format(text)
+
+def get_quote():
+    r = requests.get('http://api.forismatic.com/api/1.0/json?method=getQuote&lang=en&format=json')
+    s = r._content.decode('unicode_escape')
+    s = s.replace('\\', '')
+    r._content = str.encode(s, 'ascii', 'ignore')
+    quote = r.json()['quoteText']
+    return quote
 
 def get_wish():
     current_time = datetime.datetime.utcnow()
@@ -39,4 +94,6 @@ if __name__ == '__main__':
         verify = str(os.environ.get('ASK_VERIFY_REQUESTS', '')).lower()
         if verify == 'false':
             app.config['ASK_VERIFY_REQUESTS'] = False
-    app.run()
+    port = int(os.getenv('PORT', 5000))
+    print('Starting app on port ' + str(port))
+    app.run(port = port, host = '0.0.0.0') 
--- webservice/test.py
+++ webservice/test.py
@@ -3,6 +3,7 @@ import unittest
 import json
 import os
 
+
 class GreetingTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -15,17 +16,42 @@ class GreetingTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_hello_intent(self):
+    def test_00_hello_intent(self):
         with open('event.json') as data_file:
             jdata = json.load(data_file)
 
         answer = self.app.post('/alexa_endpoint', data=json.dumps(jdata), content_type='application/json')
-        print (answer.data)
         resp = json.loads(answer.data)
 
         self.assertTrue(resp['response']['shouldEndSession'])
         self.assertRegex(resp['response']['outputSpeech']['ssml'], r'<speak>Hello.*</speak>')
 
+    def test_01_quote_intent(self):
+        with open('quote.json') as data_file:
+            jdata = json.load(data_file)
+
+        answer = self.app.post('/alexa_endpoint', data=json.dumps(jdata), content_type='application/json')
+        resp = json.loads(answer.data)
+
+        self.assertRegex(resp['response']['outputSpeech']['ssml'], r'.*Do you want to listen to one more.*')
+
+    def test_02_next_quote_intent(self):
+        with open('next_quote.json') as data_file:
+            jdata = json.load(data_file)
+
+        answer = self.app.post('/alexa_endpoint', data=json.dumps(jdata), content_type='application/json')
+        resp = json.loads(answer.data)
+
+        self.assertRegex(resp['response']['outputSpeech']['ssml'], r'.*Wrong invocation of this intent. Please say get me a quote to get quote.*')
+
+    def test_03_next_quote_intent(self):
+        with open('next_quote_attr.json') as data_file:
+            jdata = json.load(data_file)
+
+        answer = self.app.post('/alexa_endpoint', data=json.dumps(jdata), content_type='application/json')
+        resp = json.loads(answer.data)
+
+        self.assertRegex(resp['response']['outputSpeech']['ssml'], r'.*Do you want to listen to one more quote.*')
 
 if __name__ == '__main__':
     unittest.main()
